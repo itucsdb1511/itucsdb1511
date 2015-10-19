9d6dc11ac45b5ef27add16403ee2d22257e73fe3
Tunahan Erkoyuncu <erkoyuncu@itu.edu.tr>
2015-10-19 17:16:09 +0300

    Updating navigation bar links

--- server.py
+++ server.py
@@ -23,6 +23,11 @@ def riderlist():
     now = datetime.datetime.now()
     return render_template('riderlist.html', current_time=now.ctime())
     
+@app.route('/home')
+def home():
+    now = datetime.datetime.now()
+    return render_template('home.html', current_time=now.ctime())
+    
 if __name__ == '__main__':
     PORT = int(os.getenv('VCAP_APP_PORT', '5000'))
     app.run(host='0.0.0.0', port=int(PORT))
